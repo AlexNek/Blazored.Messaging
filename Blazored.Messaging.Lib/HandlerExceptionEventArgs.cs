7e67823faff18205f4208f0844b0c992ffc6ce29
AlexNek <alexnek@russinger.com>
2025-03-16 20:34:13 +0100

    missing file

---
+++ Blazored.Messaging.Lib/HandlerExceptionEventArgs.cs
@@ -0,0 +1,20 @@
+﻿namespace Blazor.Messaging;
+
+public class HandlerExceptionEventArgs : EventArgs
+{
+    public Exception Exception { get; }
+
+    public Type MessageType { get; }
+
+    public string SubscriberInfo { get; } // Format: "ClassName.FunctionName"
+
+    public HandlerExceptionEventArgs(string subscriberInfo, Exception exception, Type messageType)
+    {
+        SubscriberInfo = subscriberInfo;
+        Exception = exception;
+        MessageType = messageType;
+    }
+
+    public override string ToString() =>
+        $"Error in {SubscriberInfo} for {MessageType.Name}: {Exception.Message}";
+}
